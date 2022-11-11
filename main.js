51f346d03307baaccf2594839c40453a9b5a9cc1
hroque01 <hadson.roque01@gmail.com>
2022-11-11 16:50:55 +0100

    array con immagini

---
+++ main.js
@@ -0,0 +1,19 @@
+// dichiariamo le variabili delle foto
+let immagini = [];
+
+const immagine1 = document.getElementById('img1')
+const immagine2 = document.getElementById('img2');
+const immagine3 = document.getElementById('img3');
+const immagine4 = document.getElementById('img4');
+const immagine5 = document.getElementById('img5');
+
+
+immagini.push(immagine1,immagine2,immagine3,immagine4,immagine5)
+
+console.log(immagini,)
+
+
+
+
+
+
